f4d1bb9204644d56f0d4596a3a3ab4e9ea5c4006
AliNinjaTfue69 <57469969+AliNinjaTfue69@users.noreply.github.com>
2020-06-06 09:43:42 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 857d937af044f3557fb8fdcff6bedcbe0b1bf84a, reversing
    changes made to 04efd5bbedb6643b447866316a6f0a04568ab2bc.
    
    # Conflicts:
    #	app/build.gradle
    #	build.gradle

--- app/src/main/java/com/example/servezy/MainActivity.java
+++ app/src/main/java/com/example/servezy/MainActivity.java
@@ -7,22 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-
 public class MainActivity extends AppCompatActivity {
     private Button next;
-    private Button signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         next = findViewById(R.id.welcomeButton);
-        next.setOnClickListener(new View.OnClickListener())
-                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                        .requestEmail()
-                        .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        {
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openMainMenu();
