2b75bfcc8f7fd2bac3acee63f95749bf774ba839
GirishJayakumar <girish.jayakumar18@gmail.com>
2020-06-05 20:13:39 -0400

    g

--- app/src/main/java/com/example/servezy/MainActivity.java
+++ app/src/main/java/com/example/servezy/MainActivity.java
@@ -11,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+    public static void main(String[] args) {
+        System.out.println("Grgtejgrdg");
+    }
+
+
+    }
+
