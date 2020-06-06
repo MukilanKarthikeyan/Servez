ad297a27baa03273ae66e6b14c2911bedc0badbe
AliNinjaTfue69 <57469969+AliNinjaTfue69@users.noreply.github.com>
2020-06-06 13:43:52 -0400

    almost done with sign in

--- app/src/main/java/com/example/servezy/MainActivity.java
+++ app/src/main/java/com/example/servezy/MainActivity.java
@@ -1,19 +1,46 @@
 package com.example.servezy;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+    SignInButton signInButton;
+    private GoogleApiClient googleApiClient;
+    private static final int SIGN_IN = 1;
     private Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInoptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this)
+            .addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+        signInButton = findViewById(R.id.google_sign_in);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, SIGN_IN);
+            }
+        }{
+
+        });
         next = findViewById(R.id.welcomeButton);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,4 +53,27 @@ public class MainActivity extends AppCompatActivity {
         Intent openMain= new Intent(this, SignIn.class );
         startActivity(openMain);
     }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if(result.isSuccess()){
+                startActivity(new Intent(MainActivity.this , ProfileActivity.class));
+                finish();
+            } else{
+                Toast.makeText(this, "Login Failed. Please try again.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
+
