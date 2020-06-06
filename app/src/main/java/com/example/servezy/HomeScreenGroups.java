b7006f3ad51fefb36f1612f0e343813187c2dc38
Mukilan <mukilankarthikeyan357@gmail.com>
2020-06-06 15:01:05 -0400

    changed auth process, now located in the HomeScreenGroups file

--- app/src/main/java/com/example/servezy/HomeScreenGroups.java
+++ app/src/main/java/com/example/servezy/HomeScreenGroups.java
@@ -3,6 +3,10 @@ package com.example.servezy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeScreenGroups extends AppCompatActivity {
 
@@ -10,5 +14,44 @@ public class HomeScreenGroups extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen_groups);
+        s
+
+        if(FirebaseAuth.getInstance().getCurrentUser()== null){
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .build(),
+                    SIGN_IN_REQUEST_CODE
+            );
+        }
+        else{
+            Toast.makeText(this,
+                    "Welcome" + FirebaseAuth.getInstance()
+                            .getCurrentUser()
+                            .getDisplayName(),
+                    Toast.LENGTH_LONG).show();
+            displayChatMessages();
+        }
+
+
+        private void displayChatMessages(){
+
+        }
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+            if(requestCode==SIGN_IN_REQUEST_CODE){
+                if(resultCode == RESULT_OK){
+                    Toast.makeText(this,"Successfully signed in. Welcome!",
+                            Toast.LENGTH_LONG).show();
+                    displayChatMessages();
+                } else {
+                    Toast.makeText(this,
+                            "We couldn't sign you in. Please try again later",
+                            Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/servezy/MainActivity.java
+++ app/src/main/java/com/example/servezy/MainActivity.java
@@ -16,46 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        if(FirebaseAuth.getInstance().getCurrentUser()== null){
-            startActivityForResult(
-                    AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .build(),
-                    SIGN_IN_REQUEST_CODE
-            );
-        }
-        else{
-            Toast.makeText(this,
-                    "Welcome" + FirebaseAuth.getInstance()
-                        .getCurrentUser()
-                        .getDisplayName(),
-                    Toast.LENGTH_LONG).show();
-            displayChatMessages();
-        }
-
-
-        private void displayChatMessages(){
-
-        }
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data){
-            super.onActivityResult(requestCode, resultCode, data);
-            if(requestCode==SIGN_IN_REQUEST_CODE){
-                if(resultCode == RESULT_OK){
-                    Toast.makeText(this,"Successfully signed in. Welcome!",
-                            Toast.LENGTH_LONG).show();
-                    displayChatMessages();
-                } else {
-                    Toast.makeText(this,
-                            "We couldn't sign you in. Please try again later",
-                            Toast.LENGTH_LONG).show();
-                    finish();
-                }
-        }
-        }
         next = findViewById(R.id.welcomeButton);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
